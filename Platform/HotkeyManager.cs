a7093804d19485e1035448ec875938c3126edff1
alfredoTous <adtous@uninorte.edu.co>
2026-07-22 12:08:13 -0500

    Add KeyState and move actions from KeyRelease to KeyPress

--- Platform/HotkeyManager.cs
+++ Platform/HotkeyManager.cs
@@ -12,6 +12,35 @@ class HotkeyManager
     static int f3_keycode;
     static int f4_keycode;
 
+    private static class KeyState
+    {
+        public static bool f1Down = false;
+        public static bool f2Down = false;
+        public static bool f3Down = false;
+        public static bool f4Down = false;
+       
+        // When F1+F2+F3+F4 are pressed we activate/deactivate the key global grabbing
+        public static bool combo => (KeyState.f1Down && KeyState.f2Down && KeyState.f3Down && KeyState.f4Down);
+        public static bool toggleComboActive = false;
+
+        public static bool CheckCombo()
+        {
+            if (KeyState.combo)
+            {
+                if (!KeyState.toggleComboActive)
+                {
+                    KeyState.toggleComboActive = true;
+                    return true;
+                }
+            }
+            else
+            {
+                KeyState.toggleComboActive = false;
+            }
+            return false;
+        }
+    }
+
     // Main Thread
     public static void ListenForKeyPress(NetworkManager network)
     {
@@ -87,12 +116,20 @@ class HotkeyManager
     {
         switch (ev.type)
         {
-            case KeyRelease:
-            {
+            case KeyPress:
+            { 
+                UpdateKeyState(ev.xkey, true);
+
+                if (KeyState.CheckCombo())
+                {
+                    Console.WriteLine("TOGGLE!");
+                    return;
+                }
                 // Buffer 1
                 if (ev.xkey.keycode == f1_keycode) 
                 { 
                     // Triggers SelectionNotify event (XConvertSelection Api)
+                    Console.WriteLine("F1 ACTION");
                     clipboard.RequestSelectionContent("PRIMARY", 1);
                     // This usually triggers 2 SelectionNotify events, first to get the targets (formats we support) and the other with the actual content of the selection (in this case PRIMARY)
                     // When the actual content of the selection is got, clipboard.HandleSelectionNotify internally invokes PrimaryContentReceived (event we subscribe) 
@@ -102,7 +139,8 @@ class HotkeyManager
 
                 if (ev.xkey.keycode == f2_keycode)
                 {
-                    // Being pasting of buffer content
+                    // Begin pasting of buffer content
+                    Console.WriteLine("F2 ACTION");
                     clipboard.BeginPasteBufferContent(1);
                     // It requests CLIPBOARD content for backup which triggers a SelectionNotify event
                     // Then it simulates CTRL+SHIFT+V for pasting
@@ -113,14 +151,22 @@ class HotkeyManager
                 // Buffer 2
                 if (ev.xkey.keycode == f3_keycode) 
                 {
+                    Console.WriteLine("F3 ACTION");
                     clipboard.RequestSelectionContent("PRIMARY", 2);
                 }
 
                 if (ev.xkey.keycode == f4_keycode)
                 {
+                    Console.WriteLine("F4 ACTION");
                     clipboard.BeginPasteBufferContent(2);
                 }
 
+                break;
+            }
+
+            case KeyRelease:
+            {
+                UpdateKeyState(ev.xkey, false);
                 break;
             }
 
@@ -141,6 +187,17 @@ class HotkeyManager
     }
 
 
+    private static void UpdateKeyState(XKeyEvent key, bool isPressed)
+    {
+        if (key.keycode == f1_keycode)
+            KeyState.f1Down = isPressed;
+        if (key.keycode == f2_keycode)
+            KeyState.f2Down = isPressed;
+        if (key.keycode == f3_keycode)
+            KeyState.f3Down = isPressed;
+        if (key.keycode == f4_keycode)
+            KeyState.f4Down = isPressed;
+    }
 
     // Simulates key strokes for pasting
     public static void SimulateCtrlShiftV(IntPtr display)
@@ -148,6 +205,8 @@ class HotkeyManager
         uint ctrl  = (uint)XKeysymToKeycode(display, XK_Control_L);
         uint shift = (uint)XKeysymToKeycode(display, XK_Shift_L);
         uint v     = (uint)XKeysymToKeycode(display, XK_V);
+        
+        XTestFakeKeyEvent(display, (uint)f2_keycode, false, 0); // Toca hacer esto para que el paste funcione
 
         XTestFakeKeyEvent(display, ctrl, true, 0);   // Ctrl Down
         XTestFakeKeyEvent(display, shift, true, 0);  // Shift Down
@@ -157,6 +216,7 @@ class HotkeyManager
         XTestFakeKeyEvent(display, shift, false, 0); // Shift Up
         XTestFakeKeyEvent(display, ctrl, false, 0);  // Ctrl Up
 
+        // XTestFakeKeyEvent(display, (uint)f2_keycode, true, 0); // Activar esto haria funcionar pero generaria un bucle infinito
         XFlush(display);
     }
 
